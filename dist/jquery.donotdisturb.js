9bbee7a55695fa3fd669c438e7846b8a709090d6
yahiousun <yahiousun@gmail.com>
2015-10-03 13:38:44 +0800

    add show/hide method

--- dist/jquery.donotdisturb.js
+++ dist/jquery.donotdisturb.js
@@ -1,22 +1,24 @@
-/*! donotdisturb - v0.0.1 - 2015-10-02
+/*! donotdisturb - v0.0.1 - 2015-10-03
 * Copyright (c) 2015 yahiousun; Licensed MIT */
 ;(function (factory) {
   'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
-}(function($, undefined){
+  if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(['jquery'], factory);
+  } else {
+      // Browser globals
+      factory(jQuery);
+  }
+} (function ($, undefined) {
   'use strict';
+
   var Donotdisturb = function (element, options) {
     this.$element = $(element).is(document.body) ? $(window) : $(element);
     this.$irrelevant = $(options.selector);
     this.options = options;
     this.initialize();
   };
+
   Donotdisturb.prototype = {
     constructor: Donotdisturb,
     istyping: false,
@@ -24,7 +26,7 @@
     offset: 0,
     initialize: function () {
       var that = this;
-      if (!that.$element.length || !that.$irrelevant.length) {
+      if (!that.$element.length) {
         return;
       }
       if (that.options.transition === true) {
@@ -53,7 +55,11 @@
       if (!that.active) {
         return;
       }
-      that.$irrelevant.css('display', 'block');
+      that.$irrelevant = $(that.options.selector);
+      if (!that.$irrelevant.length) {
+        return;
+      }
+      that.$irrelevant.attr('aria-hidden', 'false').css('display', 'block');
       if (that.options.transition && $.support.transition) {
         that.$irrelevant[0].offsetWidth; // force reflow
         that.$irrelevant.off($.support.transition.end + '.donotdisturb').addClass(that.options.transition);
@@ -65,13 +71,17 @@
       if (that.active) {
         return;
       }
+      that.$irrelevant = $(that.options.selector);
+      if (!that.$irrelevant.length) {
+        return;
+      }
       if (that.options.transition && $.support.transition) {
         that.$irrelevant.one($.support.transition.end + '.donotdisturb', function () {
-          that.$irrelevant.css('display', 'none');
+          that.$irrelevant.attr('aria-hidden', 'true').css('display', 'none');
         });
         that.$irrelevant.removeClass(that.options.transition);
       } else {
-        that.$irrelevant.removeClass(that.options.transition).css('display', 'none');
+        that.$irrelevant.attr('aria-hidden', 'true').css('display', 'none');
       }
       that.active = true;
     },
@@ -109,14 +119,44 @@
       }
     }
   };
-  $.fn.donotdisturb = function (options) {
-    options = $.extend({}, $.fn.donotdisturb.options, options);
-    return this.each(function () {
-      if (!$(this).data('donotdisturb')) {
-        $(this).data('donotdisturb', new Donotdisturb(this, options));
-      }
-    });
+
+  var methods = {
+    initialize: function (options) {
+      options = $.extend({}, $.fn.donotdisturb.options, options);
+      return this.each(function () {
+        if (!$(this).data('donotdisturb')) {
+          $(this).data('donotdisturb', new Donotdisturb(this, options));
+        }
+      });
+    },
+    show: function () {
+      return this.each(function () {
+        var $this = $(this);
+        if ($this.data('donotdisturb') instanceof Donotdisturb) {
+          $this.data('donotdisturb').show();
+        }
+      });
+    },
+    hide: function () {
+      return this.each(function () {
+        var $this = $(this);
+        if ($this.data('donotdisturb') instanceof Donotdisturb) {
+          $this.data('donotdisturb').hide();
+        }
+      });
+    }
   };
+
+  $.fn.donotdisturb = function (method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if(typeof method === 'object' || !method) {
+      return methods.initialize.apply(this, arguments);
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.donotdisturb');
+    }
+  };
+
   // default options
   $.fn.donotdisturb.options = {
     selector: '[data-irrelevant="donotdisturb"]',
@@ -124,6 +164,7 @@
     typingspy: true,
     transition: 'in'
   };
+
   $(document).ready(function () {
     // data api
     $('[data-spy="donotdisturb"]').each(function () {
--- src/donotdisturb.js
+++ src/donotdisturb.js
@@ -1,20 +1,22 @@
 ;(function (factory) {
   'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
-}(function($, undefined){
+  if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(['jquery'], factory);
+  } else {
+      // Browser globals
+      factory(jQuery);
+  }
+} (function ($, undefined) {
   'use strict';
+
   var Donotdisturb = function (element, options) {
     this.$element = $(element).is(document.body) ? $(window) : $(element);
     this.$irrelevant = $(options.selector);
     this.options = options;
     this.initialize();
   };
+
   Donotdisturb.prototype = {
     constructor: Donotdisturb,
     istyping: false,
@@ -77,7 +79,7 @@
         });
         that.$irrelevant.removeClass(that.options.transition);
       } else {
-        that.$irrelevant.removeClass(that.options.transition).attr('aria-hidden', 'true').css('display', 'none');
+        that.$irrelevant.attr('aria-hidden', 'true').css('display', 'none');
       }
       that.active = true;
     },
@@ -115,14 +117,44 @@
       }
     }
   };
-  $.fn.donotdisturb = function (options) {
-    options = $.extend({}, $.fn.donotdisturb.options, options);
-    return this.each(function () {
-      if (!$(this).data('donotdisturb')) {
-        $(this).data('donotdisturb', new Donotdisturb(this, options));
-      }
-    });
+
+  var methods = {
+    initialize: function (options) {
+      options = $.extend({}, $.fn.donotdisturb.options, options);
+      return this.each(function () {
+        if (!$(this).data('donotdisturb')) {
+          $(this).data('donotdisturb', new Donotdisturb(this, options));
+        }
+      });
+    },
+    show: function () {
+      return this.each(function () {
+        var $this = $(this);
+        if ($this.data('donotdisturb') instanceof Donotdisturb) {
+          $this.data('donotdisturb').show();
+        }
+      });
+    },
+    hide: function () {
+      return this.each(function () {
+        var $this = $(this);
+        if ($this.data('donotdisturb') instanceof Donotdisturb) {
+          $this.data('donotdisturb').hide();
+        }
+      });
+    }
   };
+
+  $.fn.donotdisturb = function (method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if(typeof method === 'object' || !method) {
+      return methods.initialize.apply(this, arguments);
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.donotdisturb');
+    }
+  };
+
   // default options
   $.fn.donotdisturb.options = {
     selector: '[data-irrelevant="donotdisturb"]',
@@ -130,6 +162,7 @@
     typingspy: true,
     transition: 'in'
   };
+
   $(document).ready(function () {
     // data api
     $('[data-spy="donotdisturb"]').each(function () {
